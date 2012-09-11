44b1efb7b7f5e8363d03ec769bc893f4dbf48589
Diego Fernando Bolivar <diegofernando83@gmail.com>
2012-09-11 12:58:03 +0200

    Paginas fundaciones objetivos y efi

--- Alianza/urls.py
+++ Alianza/urls.py
@@ -7,15 +7,22 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'website.views.home'),
+     url(r'^primera-vez/$', 'website.views.primera_vez'), 
+    url(r'^peticiones/$', 'website.views.peticiones'),
+    url(r'^sedes/$', 'website.views.sedes'),
+
     url(r'^nosotros/$', 'website.views.nosotros'),
     url(r'^vision/$', 'website.views.vision'),
     url(r'^vision/creemos/$', 'website.views.creemos'),
     url(r'^nosotros/simbolos/$', 'website.views.simbolos'),
+    url(r'^nosotros/fundaciones/$', 'website.views.fundaciones'),
+    url(r'^nosotros/objetivos/$', 'website.views.objetivos'),
+    url(r'^nosotros/efi/$', 'website.views.efi'),
+
     url(r'^mision/$', 'website.views.mision'),
+
     url(r'^noticias/(?P<noticia_slug>.+?)/?$', 'website.views.detalle_noticia'),
-    url(r'^primera-vez/$', 'website.views.primera_vez'), 
-    url(r'^peticiones/$', 'website.views.peticiones'),
-    url(r'^sedes/$', 'website.views.sedes'),
+   
     url(r'^update/?$', 'github.views.update'),
 
     #url(r'^estadistica/$', 'estadistica.views.shell'),
--- website/views.py
+++ website/views.py
@@ -14,7 +14,7 @@ from website.models import  Noticia, Youtube, Iglesia
 from website.forms import PeticionesForm
 
 
-
+##################  INICIO ##########################
 
 def home(request):
 	noticias = Noticia.objects.all().order_by('-fecha').filter(activado=True)[1:5]
@@ -24,6 +24,20 @@ def home(request):
 		video = None
 	
 	return render_to_response('website/home.html',{'noticias':noticias, 'youtube':video}, context_instance=RequestContext(request))
+
+def peticiones(request):
+	titulo = 'Peticiones de<br/> <span>Oración</span>'
+ 	form = PeticionesForm(request.POST or None)
+ 	if form.is_valid():
+ 		titulo = 'Petición de oración desde web Alianza'
+ 		content = form.cleaned_data['peticion'] + "\n"
+ 		content += 'responder a: ' + form.cleaned_data['correo'] + "\n"
+ 		content += 'de: ' + form.cleaned_data['nombre'] + "\n"
+ 		content += 'vive en: ' + form.cleaned_data['ciudad'] 
+ 		correo = EmailMessage(titulo, content, to=['diegofernando83@gmail.com'])
+ 		correo.send()
+ 		return HttpResponseRedirect('/peticiones')
+ 	return render_to_response('website/peticiones.html', {'form':form, 'titulo':titulo}, context_instance=RequestContext(request))
 
 def primera_vez(request):
 	titulo = '¿Primera vez<br/> <span>en este sitio?</span>'
@@ -37,6 +51,9 @@ def sedes(request):
 		sedes = serializers.serialize("json", q)
 		return HttpResponse(sedes, mimetype='application/json') 
 	return render_to_response('website/sedes.html', {'titulo':titulo}, context_instance=RequestContext(request))
+
+
+##################  NOSOTROS ##########################
 
 def nosotros(request):
 	imag = 'reloj1.jpg'
@@ -55,25 +72,34 @@ def simbolos(request):
 	titulo = 'Conoce más<br/> <span class="mediana">de la Alianza...</span>'
 	return render_to_response('website/simbolos.html',{'titulo':titulo}, context_instance=RequestContext(request))
 
+def fundaciones(request):
+	titulo = 'Conoce más<br/> <span class="mediana">de la Alianza...</span>'
+	return render_to_response('website/fundaciones.html',{'titulo':titulo}, context_instance=RequestContext(request))
+
+def objetivos(request):
+	titulo = 'Conoce más<br/> <span class="mediana">de la Alianza...</span>'
+	return render_to_response('website/objetivos.html',{'titulo':titulo}, context_instance=RequestContext(request))
+
+def objetivos(request):
+	titulo = 'Conoce más<br/> <span class="mediana">de la Alianza...</span>'
+	return render_to_response('website/objetivos.html',{'titulo':titulo}, context_instance=RequestContext(request))
+
+def efi(request):
+	titulo = 'Conoce más<br/> <span class="mediana">de la Alianza...</span>'
+	return render_to_response('website/efi.html',{'titulo':titulo}, context_instance=RequestContext(request))
+
+
+##################  MISION ##########################
+
 def mision(request):
 	titulo = 'Nuestra<br/> <span>Misión</span>'
 	return render_to_response('website/mision.html',{'titulo':titulo}, context_instance=RequestContext(request))
+
+##################  NOTICIAS ##########################
 
 def detalle_noticia(request, noticia_slug):
 	dato = get_object_or_404(Noticia, slug=noticia_slug)
 	return render_to_response('website/noticia.html', {'noticia':dato}, context_instance=RequestContext(request))
 
-def peticiones(request):
-	titulo = 'Peticiones de<br/> <span>Oración</span>'
- 	form = PeticionesForm(request.POST or None)
- 	if form.is_valid():
- 		titulo = 'Petición de oración desde web Alianza'
- 		content = form.cleaned_data['peticion'] + "\n"
- 		content += 'responder a: ' + form.cleaned_data['correo'] + "\n"
- 		content += 'de: ' + form.cleaned_data['nombre'] + "\n"
- 		content += 'vive en: ' + form.cleaned_data['ciudad'] 
- 		correo = EmailMessage(titulo, content, to=['diegofernando83@gmail.com'])
- 		correo.send()
- 		return HttpResponseRedirect('/peticiones')
- 	return render_to_response('website/peticiones.html', {'form':form, 'titulo':titulo}, context_instance=RequestContext(request))
+
 
